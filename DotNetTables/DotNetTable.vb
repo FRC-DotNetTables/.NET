4cf334f730de50985b3166b162805baee25aec6e
shandamelcher <git@shananda.com>
2014-01-03 19:30:11 -0800

    Initial commit

---
+++ DotNetTables/DotNetTable.vb
@@ -0,0 +1,256 @@
+﻿Imports edu.wpi.first.wpilibj
+Imports edu.wpi.first.wpilibj.tables
+Imports edu.wpi.first.wpilibj.networktables2.type
+
+
+
+Public Class DotNetTable
+    Implements ITableListener
+
+    Public Const STALE_FACTOR As Double = 2.5
+    Public Const UPDATE_INTERVAL As String = "_UPDATE_INTERVAL"
+    Private _name As String
+    Private _updateInterval As Integer
+    Private _writable As Boolean
+    Public _data As Dictionary(Of String, String)
+    Private changeCallback As DotNetTableEvents
+    Private staleCallback As DotNetTableEvents
+    Private _lastUpdate As Long
+
+
+    Public Sub New(name As String, writable As Boolean)
+        Me._lastUpdate = 0
+        Me._name = name
+        Me._writable = writable
+        Me._updateInterval = -1
+        Me.changeCallback = Nothing
+        Me.staleCallback = Nothing
+        _data = New Dictionary(Of String, String)
+    End Sub
+
+    Public ReadOnly Property name As String
+        Get
+            name = _name
+        End Get
+    End Property
+
+    Public Function isStale() As Boolean
+        'Tables with no update interval are never stale
+        If _updateInterval <= 0 Then
+            Return False
+        End If
+
+        'Tables are stale when we miss STALE_FACTOR update intervals
+        Dim age As Double = Now.Millisecond - _lastUpdate
+        If age > _updateInterval * STALE_FACTOR Then
+            Return True
+        End If
+
+        'Otherwise we're fresh
+        Return False
+
+    End Function
+
+
+    Public ReadOnly Property lastUpdate As Double
+        Get
+            lastUpdate = _lastUpdate
+        End Get
+    End Property
+
+
+    Public ReadOnly Property iswritable As Boolean
+        Get
+            iswritable = _writable
+        End Get
+    End Property
+
+
+    Private Sub throwIfNotWritable()
+        If _writable = False Then
+            Throw New IllegalStateException("Table is read-only: " & _name)
+        End If
+    End Sub
+
+
+    Public ReadOnly Property getInterval As Integer
+        Get
+            getInterval = _updateInterval
+        End Get
+    End Property
+
+
+
+    Public Sub setInterval(update As Integer)
+        throwIfNotWritable()
+        If update <= 0 Then
+            update = -1
+        End If
+        _updateInterval = update
+    End Sub
+
+    Public Sub onChange(callback As DotNetTableEvents)
+        changeCallback = callback
+    End Sub
+
+
+    Public Sub onStale(callback As DotNetTableEvents)
+        If _writable Then
+            Throw New IllegalStateException("Table is local: " & _name)
+        End If
+        staleCallback = callback
+        Throw New UnsupportedOperationException("Not supported yet.")
+    End Sub
+
+    Public Sub clear()
+        _data.Clear()
+    End Sub
+
+
+    Public ReadOnly Property Keys As Dictionary(Of String, String).KeyCollection
+        Get
+            Keys = _data.Keys
+        End Get
+    End Property
+
+
+
+    Public ReadOnly Property exists(key As String) As Boolean
+        Get
+            exists = _data.ContainsKey(key)
+        End Get
+    End Property
+
+
+    Public Sub setVal(key As String, value As String)
+        throwIfNotWritable()
+        If _data.ContainsKey(key) Then
+            _data.Item(key) = value
+        Else
+            _data.Add(key, value)
+        End If
+        _lastUpdate = Now.Millisecond
+    End Sub
+
+    Public Sub setVal(key As String, value As Double)
+        setVal(key, value.ToString)
+    End Sub
+
+    Public Sub setVal(key As String, value As Integer)
+        setVal(key, value.ToString)
+    End Sub
+
+
+    Public Function getVal(key As String) As String
+        getVal = _data.Item(key)
+    End Function
+
+    Public Function getDouble(key As String) As Double
+        Double.TryParse(_data.Item(key), getDouble)
+    End Function
+
+    Public Function getInt(key As String) As Integer
+        Integer.TryParse(_data.Item(key), getInt)
+    End Function
+
+
+    Public Sub remove(key As String)
+        throwIfNotWritable()
+        _data.Remove(key)
+    End Sub
+
+
+
+
+
+    Private Sub recv(value As StringArray)
+        'unpack the new data
+        _data = SAtoHM(value)
+        _lastUpdate = Now.Millisecond
+
+        'note the published update interval
+        If exists(UPDATE_INTERVAL) Then
+            _updateInterval = getInt(UPDATE_INTERVAL)
+        End If
+
+        'dispatch our callback, if any
+        If changeCallback IsNot Nothing Then
+            changeCallback.changed(Me)
+        End If
+    End Sub
+
+
+    Public Sub send()
+        throwIfNotWritable()
+        setVal(UPDATE_INTERVAL, getInterval())
+        DotNetTables.push(name, HMtoSA(_data))
+
+        'Dispatch our callback, if any
+        If (changeCallback IsNot Nothing) Then
+            changeCallback.changed(Me)
+        End If
+
+    End Sub
+
+
+
+    Private Function HMtoSA(data As Dictionary(Of String, String)) As StringArray
+        Dim out As New StringArray
+        For Each key In data.Keys
+            out.Add(key)
+        Next
+
+        For Each key In data
+            out.add(data.Item(key))
+        Next
+
+        Return out
+    End Function
+
+
+    Private Function SAtoHM(data As StringArray) As Dictionary(Of String, String)
+        Dim out As New Dictionary(Of String, String)
+
+        If data.size Mod 2 <> 0 Then
+            Throw New ArrayIndexOutOfBoundsException("StringArray contains an odd number of elements")
+        End If
+
+        Dim setSize As Integer = data.size / 2
+        For i = 0 To setSize
+            out.Add(data.get(i), data.get(i + setSize))
+        Next
+        SAtoHM = out
+    End Function
+
+
+    '* Update with new data from a remote subscribed table
+    '*
+    '* @param itable The underlying NetworkTable table
+    '* @param key The array name -- must match our name to trigger an update
+    '* @param value The new or updated array
+    '* @param isNew True if the array did not previous exist
+
+    Public Sub valueChanged(itable As ITable, key As String, val As Object, isNew As Boolean) Implements ITableListener.valueChanged
+        'skip updates for other tables
+        If _name <> key Then
+            Return
+        End If
+
+        'store the new data
+        Dim value As StringArray
+        itable.retrieveValue(key, value)
+        recv(value)
+    End Sub
+
+
+    Public Event changed(table As DotNetTable)
+    Public Event stale(table As DotNetTable)
+
+    Public Interface DotNetTableEvents
+
+        Sub changed(table As DotNetTable)
+        Sub stale(table As DotNetTable)
+
+    End Interface
+
+End Class
---
+++ DotNetTables/DotNetTables.vb
@@ -0,0 +1,171 @@
+﻿Imports System.Text.RegularExpressions
+Imports edu.wpi.first.wpilibj
+Imports edu.wpi.first.wpilibj.networktables
+Imports edu.wpi.first.wpilibj.networktables.NetworkTable
+Imports System.IO
+
+Public Class DotNetTables
+
+
+
+    'The table name used for the underlying NetworkTable
+    Public Const TABLE_NAME As String = "DotNet"
+    Private nt_table As NetworkTable
+    Private client As Boolean = False
+    Private connected As Boolean = False
+    Private DotNetTable() As ArrayList
+    Private tables As List(Of DotNetTable)
+
+    Private Sub init()
+        tables = New List(Of DotNetTable)
+
+        'Attempt to init the underlying NetworkTable
+        Try
+
+            NetworkTable.initialize()
+            nt_table = NetworkTable.getTable(TABLE_NAME)
+            connected = True
+        Catch ex As IOException
+            Debug.Print("Unable to initialize NetworkTable: " & TABLE_NAME)
+            Throw ex
+        End Try
+
+    End Sub
+
+    Public Sub startServer()
+        init()
+    End Sub
+
+
+    Public Sub startClient(IP As String)
+        NetworkTable.setClientMode()
+
+        If Regex.Match(IP, "^\\d{4}$").Success Then
+            NetworkTable.setTeam(CInt(IP))
+        ElseIf Regex.Match(IP, "^(?:\\d{1,3}.){3}\\d{1,3}$").Success Then
+            NetworkTable.setIPAddress(IP)
+        Else
+            Throw New IllegalArgumentException("Invalid IP address or team number: " & IP)
+        End If
+
+        client = True
+        init()
+    End Sub
+
+
+
+    '/**
+    '* @return True if this device is configured as a NetworkTable subscriber
+    ' */
+    Public Function isClient() As Boolean
+        Return client
+    End Function
+
+    '/**
+    ' * @return True if the connection has been successfully initialized
+    ' */
+    Public Function isConnected() As Boolean
+        Return connected
+    End Function
+
+    '/**
+    ' * Find the specified table in the subscribed tables list, if it exists
+    ' *
+    ' * @param name The table to be found
+    ' * @return The specified table, if available. NULL if no such table exists.
+    ' */
+
+    Public Function findTable(name As String) As DotNetTable
+        For Each table As DotNetTable In tables
+            If table.name = name Then
+                Return table
+            End If
+        Next
+
+        Throw New IllegalArgumentException("No such table: " & name)
+    End Function
+
+
+    '/**
+    ' * Subscribe to a table published by a remote host. Works in both server and
+    ' * client modes.
+    ' *
+    ' * @param name Name of the table to subscribe
+    ' * @return The subscribed table
+    ' */
+    Public Function subscribe(name As String) As DotNetTable
+        Return getTable(name, False)
+    End Function
+
+    '/**
+    ' * Publish a table for remote hosts. Works in both server or client modes.
+    ' *
+    ' * @param name Name of the table to publish
+    ' * @return The published table
+    ' */
+    Public Function publish(name As String) As DotNetTable
+        Return getTable(name, True)
+    End Function
+
+    '/**
+    ' * Get a table, creating and subscribing/publishing as necessary
+    ' *
+    ' * @param name New or existing table name
+    ' * @return The table to get/create
+    ' */
+    Private Function getTable(name As String, writable As Boolean) As DotNetTable
+        Dim table As DotNetTable
+        Try
+            table = findTable(name)
+        Catch ex As IllegalArgumentException
+            table = New DotNetTable(name, writable)
+            tables.Add(table)
+
+            'Publish or subscribe the new table
+            If writable = True Then
+                table.send()
+            Else
+                nt_table.addTableListener(table)
+            End If
+        End Try
+
+        'Ensure the table has the specified writable state
+        If table.iswritable() <> writable Then
+            Throw New IllegalStateException("Table already exists but does not share writable state: " & name)
+        End If
+
+        Return table
+    End Function
+
+
+    '/**
+    ' * Removes a table from the subscription/publish list
+    ' *
+    ' * @param name The table to remove
+    ' */
+    Public Sub drop(name As String)
+        Try
+            Dim table As DotNetTable = findTable(name)
+            nt_table.removeTableListener(table)
+            tables.Remove(table)
+        Catch ex As IllegalArgumentException
+            'Ignore invalid drop requests
+        End Try
+    End Sub
+
+
+    '/**
+    ' * Push the provided object into the NetworkTable
+    ' *
+    ' * @param name DotNetTable name
+    ' * @param data StringArray-packed DotNetTable data
+    ' */
+    Public Sub push(name As String, data As Object)
+        If Not isClient() Then
+            Throw New IllegalStateException("NetworkTable not initalized")
+        End If
+        nt_table.putValue(name, data)
+    End Sub
+
+
+End Class
