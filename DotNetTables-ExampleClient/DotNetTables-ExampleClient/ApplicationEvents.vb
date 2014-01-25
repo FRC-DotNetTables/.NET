d7b91f1624465cf1ea1be3af9934ade18f9d9ff6
shandamelcher <git@shananda.com>
2014-01-24 17:10:41 -0800

    Adding Dist folder

--- DotNetTables-ExampleClient/DotNetTables-ExampleClient/ApplicationEvents.vb
+++ DotNetTables-ExampleClient/DotNetTables-ExampleClient/ApplicationEvents.vb
@@ -10,7 +10,7 @@
     Partial Friend Class MyApplication
 
         Private Sub AppStart(ByVal sender As Object,
-    ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
+            ByVal e As Microsoft.VisualBasic.ApplicationServices.StartupEventArgs) Handles Me.Startup
 
             AddHandler AppDomain.CurrentDomain.AssemblyResolve, AddressOf DotNetTables.DotNetTables.ResolveAssemblies
         End Sub
