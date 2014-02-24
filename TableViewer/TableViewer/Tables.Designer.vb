7c26da3d0b7f1bf71301f5ed8cfd17253e66450f
shandamelcher <git@shananda.com>
2014-02-23 18:36:54 -0800

    Adding TableViewer project

---
+++ TableViewer/TableViewer/Tables.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Tables
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TableDGV = New System.Windows.Forms.DataGridView()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.DeleteRowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabelLast = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabelInterval = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.IntervalTxt = New System.Windows.Forms.TextBox()
+        Me.IntervalBtn = New System.Windows.Forms.Button()
+        Me.ToolStripStatusStale = New System.Windows.Forms.ToolStripStatusLabel()
+        CType(Me.TableDGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableDGV
+        '
+        Me.TableDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.TableDGV.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.TableDGV.Location = New System.Drawing.Point(12, 12)
+        Me.TableDGV.Name = "TableDGV"
+        Me.TableDGV.Size = New System.Drawing.Size(610, 323)
+        Me.TableDGV.TabIndex = 0
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DeleteRowToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(134, 26)
+        '
+        'DeleteRowToolStripMenuItem
+        '
+        Me.DeleteRowToolStripMenuItem.Name = "DeleteRowToolStripMenuItem"
+        Me.DeleteRowToolStripMenuItem.Size = New System.Drawing.Size(133, 22)
+        Me.DeleteRowToolStripMenuItem.Text = "Delete Row"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabelLast, Me.ToolStripStatusLabelInterval, Me.ToolStripStatusStale})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 345)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(634, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabelLast
+        '
+        Me.ToolStripStatusLabelLast.Name = "ToolStripStatusLabelLast"
+        Me.ToolStripStatusLabelLast.Size = New System.Drawing.Size(75, 17)
+        Me.ToolStripStatusLabelLast.Text = "Last Update: "
+        '
+        'ToolStripStatusLabelInterval
+        '
+        Me.ToolStripStatusLabelInterval.Name = "ToolStripStatusLabelInterval"
+        Me.ToolStripStatusLabelInterval.Size = New System.Drawing.Size(93, 17)
+        Me.ToolStripStatusLabelInterval.Text = "Update Interval: "
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(357, 351)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Update Interval:"
+        Me.Label1.Visible = False
+        '
+        'IntervalTxt
+        '
+        Me.IntervalTxt.Location = New System.Drawing.Point(446, 348)
+        Me.IntervalTxt.Name = "IntervalTxt"
+        Me.IntervalTxt.Size = New System.Drawing.Size(87, 20)
+        Me.IntervalTxt.TabIndex = 3
+        Me.IntervalTxt.Visible = False
+        '
+        'IntervalBtn
+        '
+        Me.IntervalBtn.Location = New System.Drawing.Point(539, 345)
+        Me.IntervalBtn.Name = "IntervalBtn"
+        Me.IntervalBtn.Size = New System.Drawing.Size(83, 23)
+        Me.IntervalBtn.TabIndex = 7
+        Me.IntervalBtn.Text = "Update"
+        Me.IntervalBtn.UseVisualStyleBackColor = True
+        Me.IntervalBtn.Visible = False
+        '
+        'ToolStripStatusStale
+        '
+        Me.ToolStripStatusStale.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStripStatusStale.ForeColor = System.Drawing.Color.Red
+        Me.ToolStripStatusStale.Name = "ToolStripStatusStale"
+        Me.ToolStripStatusStale.Size = New System.Drawing.Size(0, 17)
+        '
+        'Tables
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(634, 367)
+        Me.Controls.Add(Me.IntervalBtn)
+        Me.Controls.Add(Me.IntervalTxt)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.TableDGV)
+        Me.Name = "Tables"
+        Me.Text = "Subscribed to"
+        CType(Me.TableDGV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TableDGV As System.Windows.Forms.DataGridView
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabelLast As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabelInterval As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents IntervalTxt As System.Windows.Forms.TextBox
+    Friend WithEvents IntervalBtn As System.Windows.Forms.Button
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents DeleteRowToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripStatusStale As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ TableViewer/TableViewer/Tables.vb
@@ -0,0 +1,161 @@
+﻿Imports DotNetTables
+Imports DotNetTables.DotNetTable
+Imports DotNetTables.DotNetTables
+Imports System.IO
+Imports System.Threading
+
+Public Class Tables
+    Implements DotNetTableEvents
+
+    Public Table As DotNetTable
+    Delegate Sub UpdateDelegate(DelTable As DotNetTable)
+
+    Public Sub New(TableName As String, IsPublished As Boolean)
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        If IsPublished = True Then
+            'publish to a table
+            Table = DotNetTables.DotNetTables.publish(TableName) 'read only
+        Else
+            'subscribe to a table
+            Table = DotNetTables.DotNetTables.subscribe(TableName) 'read only
+            'register for the onStale event for subscribed tables
+            Table.onStale(Me)
+            'register for updates from the table
+            Table.onChange(Me)
+        End If
+    End Sub
+
+    Private Sub Subscribed_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Table.iswritable = True Then
+            Me.Text = "Published " & Table.name
+            'show published controls 'hiding for now
+            Label1.Visible = False
+            IntervalBtn.Visible = False
+            IntervalTxt.Visible = False
+
+            'add column headers
+            Dim Key As New DataGridViewTextBoxColumn
+            key.Name = "Key"
+            key.HeaderText = "Key"
+            key.DataPropertyName = "Key"
+            Dim Value As New DataGridViewTextBoxColumn
+            Value.Name = "Value"
+            Value.HeaderText = "Value"
+            Value.DataPropertyName = "Value"
+            TableDGV.Columns.Add(Key)
+            TableDGV.Columns.Add(Value)
+
+            'enable editing
+            TableDGV.ReadOnly = False
+        Else
+            Me.Text = "Subscribed to " & Table.name
+            'hide published controls
+            Label1.Visible = False
+            IntervalBtn.Visible = False
+            IntervalTxt.Visible = False
+            'disable editing
+            TableDGV.ReadOnly = True
+        End If
+    End Sub
+
+    Public Sub changed(EventTable As DotNetTable) Implements DotNetTableEvents.changed
+        If Me.InvokeRequired Then
+            Logs(EventTable)
+            Me.Invoke(New UpdateDelegate(AddressOf changed), EventTable)
+        Else
+            UpdateForm()
+        End If
+    End Sub
+
+    Public Sub stale(table As DotNetTable) Implements DotNetTableEvents.stale
+        Me.ToolStripStatusStale.Text = "Table is stale."
+    End Sub
+
+    Private Sub UpdateForm()
+        Dim MyArray As Array
+        MyArray = Table._data.ToArray
+        TableDGV.DataSource = MyArray
+        UpdateLabels()
+    End Sub
+
+    Private Sub UpdateLabels()
+        Dim StartDate As New DateTime(1970, 1, 1)
+        Me.ToolStripStatusLabelInterval.Text = "UpdateInterval: " & Table.getInterval
+        Me.ToolStripStatusLabelLast.Text = "Last Update: " & StartDate.AddMilliseconds(Table.lastUpdate)
+        Me.ToolStripStatusStale.Text = ""
+    End Sub
+
+    Private Sub Logs(TableData As DotNetTable)
+        Dim OutputString As String = ""
+        For Each item As String In TableData.Keys
+            OutputString = OutputString & item & "|" & TableData.getValue(item) & Environment.NewLine
+        Next
+        Dim FileName As String = Application.StartupPath & "\" & Table.name & ".txt"
+        File.AppendAllText(FileName, Environment.NewLine & Now & Environment.NewLine)
+        File.AppendAllText(FileName, OutputString)
+    End Sub
+
+    Private Sub TableDGV_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles TableDGV.CellEndEdit
+        Dim Key As String
+        Dim Value As String
+
+        Key = TableDGV.CurrentRow.Cells("Key").Value
+        Value = TableDGV.CurrentRow.Cells("Value").Value
+
+        If Key <> "" Then
+            If Table.exists(Key) = True Then
+                For Each row As DataGridViewRow In TableDGV.Rows
+                    If row.Index <> TableDGV.CurrentRow.Index Then
+                        If row.Cells("Key").Value = Key Then
+                            MsgBox("This key alredy exists.", MsgBoxStyle.Exclamation, "Duplicate Key")
+                            TableDGV.Rows.Remove(TableDGV.CurrentRow)
+                            Exit Sub
+                        End If
+                    End If
+                Next
+            End If
+
+            Table.setValue(Key, Value)
+            SendData()
+        End If
+    End Sub
+
+    Private Sub SendData()
+        Table.send()
+        UpdateLabels()
+        Logs(Table)
+    End Sub
+
+    Private Sub DeleteRowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DeleteRowToolStripMenuItem.Click
+        'remove rows
+        For Each row In TableDGV.SelectedRows
+            Try
+                Table.remove(row.Cells("Key").Value.ToString)
+            Catch ex As Exception
+            Finally
+                'want to remove row from dgv anyway
+                Try
+                    TableDGV.Rows.Remove(row)
+                Catch ex As Exception
+                    'do nothing if it fails
+                End Try
+            End Try
+        Next
+    End Sub
+
+    Private Sub Subscribed_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        DotNetTables.DotNetTables.drop(Table.name())
+    End Sub
+
+    Private Sub IntervalBtn_Click(sender As Object, e As EventArgs) Handles IntervalBtn.Click
+        Dim UpdateInterval As String
+        UpdateInterval = IntervalTxt.Text
+        If IsNumeric(UpdateInterval) Then
+            Table.setInterval(UpdateInterval)
+        Else
+            MsgBox("Update Interval must be numeric.", MsgBoxStyle.Exclamation, "Invalid Input")
+        End If
+    End Sub
+End Class
